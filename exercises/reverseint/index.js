bf2f83c47a8510557c3291ea395dca43afda03a4
anshumantsagar <anshumantsagar@gmail.com>
2020-08-09 23:58:26 +0530

    reverseInt

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,55 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+const reverse = require("../reversestring");
+
+function reverseInt(n) {
+    const reversed = n
+        .toString()
+        .split('')
+        .reverse()
+        .join('');
+
+    return parseInt(reversed) * Math.sign(n);
+}
 
 module.exports = reverseInt;
+
+// function reverseInt(n) {
+//     let newN = n.toString();
+//     newN =  newN.split('');
+//     let neg = false;
+//     let reversedN = [];
+//     if (newN[0] === '-') {
+//       neg = true;
+//       newN.splice(0,1);
+//     }
+//     for (let elem of newN) {
+//         if ( elem === '0' || 0) {
+//             newN.splice(elem,1);
+//         }
+//         else {
+//           reversedN.push(elem);
+//         }
+//     }
+//     if (neg === true ) {
+//       reversedN.push('-');
+//     }
+//     return Number(reversedN.reverse().join(''));
+//     // i know it has a flaw this it will not treat correctly any number which has '0' in between 
+//     // but it passed all the test case written so i put it here
+// }
+
+// function reverseInt(n) {
+//     const reversed = n
+//         .toString()
+//         .split('')
+//         .reverse()
+//         .join('');
+    
+//     if( n < 0 ) {
+//         return parseInt(reversed) * -1;
+//     }
+
+//     return parseInt(reversed);
+// }
