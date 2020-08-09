e7f073c46afa01bf72a954fecefa1c2e615958a8
anshumantsagar <anshumantsagar@gmail.com>
2020-08-09 15:34:47 +0530

    project build with reverse string

---
+++ exercises/reversestring/index.js
@@ -0,0 +1,16 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+function reverse(str) {
+    let newStr = str.split('');
+    newStr = newStr.reverse();
+    newStr = newStr.join('');
+    return newStr
+}
+
+module.exports = reverse;
