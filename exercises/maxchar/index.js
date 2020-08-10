887557487bd1eac8cf02ab3bfeeb6121fccc5c35
anshumantsagar <anshumantsagar@gmail.com>
2020-08-10 20:05:30 +0530

    maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,24 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const chars = {};
+    let max = 0;
+    let maxChar = '';
+    for (let char of str ) {
+        if (!chars[char]) {
+            chars[char] = 1;
+        } else {
+            chars[char]++;
+        }
+    }; 
+    for (let elem in chars) {
+        if (chars[elem] > max) {
+            max = chars[elem];
+            maxChar = elem;
+        }
+    }
+    return maxChar;
+}
 
-module.exports = maxChar;
+module.exports = maxChar; 
