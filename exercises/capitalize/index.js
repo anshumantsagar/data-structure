c1a71713894495002b2840cc3048863284de7ca2
anshumantsagar <anshumantsagar@gmail.com>
2020-08-22 15:35:26 +0530

    capitalize

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,51 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+    let result = str[0].toUpperCase();
+
+    for ( let i=1; i<str.length; i++) {
+        if (str[i-1] === ' ') {
+            result += str[i].toUpperCase();
+        } else {
+            result += str[i];
+        }
+    }
+    return result;
+}
 
 module.exports = capitalize;
+
+// function capitalize(str) {
+//     let result = str[0].toUpperCase();
+
+//     for ( let i=1; i<str.length; i++) {
+//         if (str[i-1] === ' ') {
+//             result += str[i].toUpperCase();
+//         } else {
+//             result += str[i];
+//         }
+//     }
+//     return result;
+// }
+
+// function capitalize(str) {
+//     let words = [];
+//     for (let word of str.split(' ')) {
+//         words.push(word[0].toUpperCase() + word.slice(1));
+//     }
+//     return words.join(' ');
+// }
+
+// function capitalize(str) {
+//     str = str.split('');
+//     let first = str[0].toUpperCase();
+//     str.splice(0,1,first);
+//     for (let i=0; i<str.length; i++) {
+//       if (str[i-1] === ' ') {
+//           let replaceWith = str[i].toUpperCase();
+//             str.splice(i,1,replaceWith);
+//         }
+//     }
+//     return str.join('');
+// }
