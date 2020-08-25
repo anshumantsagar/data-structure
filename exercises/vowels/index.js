0335c42fc07ae507d3eafec2ff128e5c4a98f2f9
anshumantsagar <anshumantsagar@gmail.com>
2020-08-25 18:05:26 +0530

    vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,38 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+    const matches = str.matches(/[aeiou]/gi);
+    return matches ? matches.length : 0;
+}
 
 module.exports = vowels;
+
+// function vowels(str) {
+//     let count = 0;
+//     const checker = ['a','e','i','o','u'];
+    
+//     for ( let elem of str.toLowerCase() ) {
+//         console.log(elem);
+//         if ( checker.includes(char)) {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+
+// function vowels(str) {
+//     const newStr = str.toLowerCase().split('');
+//     let numOfVowels = 0;
+//     for ( let elem of newStr ) {
+//         console.log(elem);
+//         if ( elem === 'a' ||
+//             elem === 'e' ||
+//             elem === 'i' ||
+//             elem === 'o' ||
+//             elem === 'u') {
+//         numOfVowels++;
+//         }
+//     }
+//     return numOfVowels;
+// }
