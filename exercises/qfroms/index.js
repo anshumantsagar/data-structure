f8906b86cc58cc9a3bb8791c3cbe89eb68ff3bc1
Anshumant <anshumant@logic-square.com>
2020-12-18 13:40:20 +0530

    Q from Stacks

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -14,6 +14,78 @@
 
 const Stack = require('./stack');
 
-class Queue {}
+class Queue {
+    constructor() {
+        this.stackA = new Stack();
+        this.stackB = new Stack();
+    }
+
+    add(record) {
+        this.stackA.push(record);
+    }
+
+    remove() {
+        while (this.stackA.peek()) {
+            this.stackB.push(this.stackA.pop());
+        }
+        const a = this.stackB.pop();
+        while (this.stackB.peek()) {
+            this.stackA.push(this.stackB.pop());
+        }
+        return a;
+    }
+
+    peek() {
+        let a = null;
+        while (this.stackA.peek()) {
+            this.stackB.push(this.stackA.pop());
+        }
+        a = this.stackB.peek();
+        while (this.stackB.peek()) {
+            this.stackA.push(this.stackB.pop());
+        }
+        return a;
+    }
+}
 
 module.exports = Queue;
+
+// class Queue {
+//     constructor() {
+//         this.stackA = new Stack();
+//         this.stackB = new Stack();
+//     }
+
+//     add(record) {
+//         this.stackA.push(record);
+//     }
+
+//     remove() {
+//         while (this.stackA.peek()) {
+//             this.stackB.push(this.stackA.peek());
+//             this.stackA.pop();
+//             //alternatively we can write this as 
+//             //this.stackB.push(this.stackA.pop());
+//         }
+//         const a = this.stackB.pop();
+//         while (this.stackB.peek()) {
+//             this.stackA.push(this.stackB.peek());
+//             this.stackB.pop();
+//         }
+//         return a;
+//     }
+
+//     peek() {
+//         let a = null;
+//         while (this.stackA.peek()) {
+//             this.stackB.push(this.stackA.peek());
+//             this.stackA.pop();
+//         }
+//         a = this.stackB.peek();
+//         while (this.stackB.peek()) {
+//             this.stackA.push(this.stackB.peek());
+//             this.stackB.pop();
+//         }
+//         return a;
+//     }
+// }
